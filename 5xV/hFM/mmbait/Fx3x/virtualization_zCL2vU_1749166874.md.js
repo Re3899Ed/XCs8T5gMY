76f8b17ac3b7a35286e0382ad111e1da610c9f16
Re3899Ed <pleats.ballots.0j@icloud.com>
2025-07-07 21:34:39 +0100

    Optimize the file's code structure for better scalability

--- 5xV/hFM/mmbait/Fx3x/virtualization_zCL2vU_1749166874.md.js
+++ 5xV/hFM/mmbait/Fx3x/virtualization_zCL2vU_1749166874.md.js
@@ -42,7 +42,6 @@ if (env === 'development') {
     presets: ['@babel/preset-react', '@babel/preset-flow'],
   };
 }
-if (env === 'production') {
   module.exports = {
     comments: false,
     plugins: [
